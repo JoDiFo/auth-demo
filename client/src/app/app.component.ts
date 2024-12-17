dc91834406ec1ead98d618964678b3a738bd3611
alexandr-fokin <sasafokin2@gmail.com>
2024-12-17 10:39:11 +0300

    feat: create form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {FormComponent} from "./form/form.component"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FormComponent]
 })
 export class AppComponent {
   title = 'client';
