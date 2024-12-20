8f5149da93063d2d19d5b3d1b86c402666c2b8cc
Yelizaveta <vetashapel@gmail.com>
2024-12-20 12:08:57 +0300

    Delete back_server directory

--- back_server/DBContext.cs
+++
@@ -1,9 +0,0 @@
-
-public class DBContext : DbContext
-{
-    public DBContext(DbContextOptions<DBContext> options) : base(options)
-    {
-
-    }
-    public DbSet<Users> Users { get; set; }
-}
--- back_server/UserInterface.cs
+++
@@ -1,8 +0,0 @@
-
-public class UserInterface 
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Password { get; set; }
-    public string Description {  get; set; }
-}
