2eaa1c2a9e574fcf5c424f7f48e72e6e966899c9
alexandr-fokin <sasafokin2@gmail.com>
2025-01-13 11:13:30 +0300

    feat: add get user in profile

--- client/src/app/profile-popup/profile-popup.component.ts
+++ client/src/app/profile-popup/profile-popup.component.ts
@@ -8,7 +8,12 @@ import { SERVER_URL } from '../constants';
   styleUrls: ['./profile-popup.component.css'],
 })
 export class ProfilePopupComponent implements OnInit {
-  @Input() user!: IUser | null;
+  username: string = '';
+  passwordHash: string = '';
+
+  loading: boolean = false;
+
+  @Input() token!: string | null;
 
   @Output() onLogout = new EventEmitter();
 
@@ -19,16 +24,31 @@ export class ProfilePopupComponent implements OnInit {
         'Content-Type': 'application/json;charset=utf-8',
       },
     })
-      .then((res) => {
-        this.onLogout.emit();
-
-        return res.json();
-      })
-      .then(console.log)
+      .then(() => this.onLogout.emit())
       .catch(console.error);
   }
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loading = true;
+
+    if (this.token) {
+      fetch(SERVER_URL + '/api/Auth/GetUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+      })
+        .then((res) => res.json())
+        .then((data: IUser) => {
+          this.username = data.name;
+          this.passwordHash = data.password;
+        })
+        .catch(console.error)
+        .finally(() => {
+          this.loading = false;
+        });
+    }
+  }
 }
