cbad4528cfa19c50b175ecf66d92396dc602f7f0
alexandr-fokin <sasafokin2@gmail.com>
2025-01-29 12:41:58 +0300

    fix: change endpoints

--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,9 +1,9 @@
 export const SERVER_URL = 'http://192.168.5.32:5000';
 
 export enum ESeverEndpoints {
-  LOGIN = '/api/Auth/LoginByPassword',
-  REGISTER = '/api/Registration/Registration',
-  LOGOUT = '/api/Auth/Logout',
+  LOGIN = '/api/Auth/SignIn',
+  REGISTER = '/api/Registration/SignUp',
+  LOGOUT = '/api/Auth/SignOut',
   CHECK_TOKEN_TIME = '/api/Token/CheckTokenTime',
   REFRESH_TOKEN_TIME = '/api/Token/RefreshTokenTime',
   GET_USER_DATA = '/api/Token/GetUserData',
